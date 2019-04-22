f5ef26fb111089ac6131ea725da1cb2bf5cd2dc8
Armando Padilla <armando_padilla_81@yahoo.com>
2019-04-22 09:10:17 -0700

    create a game and some of profile

--- backend/game/create-thankyou.js
+++
@@ -1,3 +0,0 @@
-module.exports = (app, req, res) => {
-  return app.render(req.raw, res.res, '/game/create-thankyou', req.query, {})
-};
--- backend/user/profile.js
+++ backend/user/profile.js
@@ -1,3 +1,28 @@
-module.exports = (app, req, res) => {
+module.exports = async (app, req, res) => {
+
+  // Check if the user is logged in or not
+
+  // Grab the token
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hbmRvcGFkaWxsYTgxQGdtYWlsLmNvbSIsImlkIjoiNWNiNzU0MGZlZmQ1Y2U1NWJhNGZjM2Y4IiwidXNlcm5hbWUiOiJhcm1hbmRvIiwiaWF0IjoxNTU1NjMzMzMyfQ.p-MYe5hNGHxRX3_63szy6o6wq50VKoZDN6yQPNDuWfI';
+
+  // Grab the game id
+  const { gameId } = req.params;
+  console.log(gameId);
+
+  // Fetch game info
+  // Fetch the data from API
+  var options = {
+    method: 'GET',
+    url: `http://localhost:3000/v1/game/${gameId}?playerInfo=1`,
+    headers: {
+      'authorization': `Bearer ${token}`
+    }
+  };
+
+  const resGame = await request(options);
+  const game = JSON.parse(resGame).data;
+  console.log("game", game);
+
+
   return app.render(req.raw, res.res, '/user/profile', req.query, {})
 };
