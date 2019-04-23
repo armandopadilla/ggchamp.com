287fc0123ad43f370c7523304540915a97de708e
Armando Padilla <armando_padilla_81@yahoo.com>
2019-04-22 17:47:13 -0700

    profile changes

--- backend/user/profile.js
+++ backend/user/profile.js
@@ -1,28 +1,24 @@
+const request = require('request-promise');
+
 module.exports = async (app, req, res) => {
 
   // Check if the user is logged in or not
 
   // Grab the token
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hbmRvcGFkaWxsYTgxQGdtYWlsLmNvbSIsImlkIjoiNWNiNzU0MGZlZmQ1Y2U1NWJhNGZjM2Y4IiwidXNlcm5hbWUiOiJhcm1hbmRvIiwiaWF0IjoxNTU1NjMzMzMyfQ.p-MYe5hNGHxRX3_63szy6o6wq50VKoZDN6yQPNDuWfI';
-
-  // Grab the game id
-  const { gameId } = req.params;
-  console.log(gameId);
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hbmRvcGFkaWxsYTgxQGdtYWlsLmNvbSIsImlkIjoiNWNiNzU0MGZlZmQ1Y2U1NWJhNGZjM2Y4IiwidXNlcm5hbWUiOiJhcm1hbmRvIiwiaWF0IjoxNTU1OTQ5NTQ1fQ.ZRru8kGP8ORcEjJCA9OKsH62QcPn7ex9xkk_U8ISy5U';
 
   // Fetch game info
   // Fetch the data from API
   var options = {
     method: 'GET',
-    url: `http://localhost:3000/v1/game/${gameId}?playerInfo=1`,
+    url: `http://localhost:3000/v1/user/profile`,
     headers: {
       'authorization': `Bearer ${token}`
     }
   };
 
-  const resGame = await request(options);
-  const game = JSON.parse(resGame).data;
-  console.log("game", game);
+  const resUserInfo = await request(options);
+  const userInfo = JSON.parse(resUserInfo).data;
 
-
-  return app.render(req.raw, res.res, '/user/profile', req.query, {})
+  return app.render(req.raw, res.res, '/user/profile', { userInfo }, {})
 };
